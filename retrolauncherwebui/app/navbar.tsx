62c5a7ad5c8460429c045b4396c534bca9919e36
Samuel Banya <sbanya@fastmail.com>
2025-12-30 22:18:55 -0600

    Separated out sections into their own components, fixed goofy flex issue with card grid

---
+++ retrolauncherwebui/app/navbar.tsx
@@ -0,0 +1,67 @@
+export default function NavBar() {
+    return (
+      <div className="navbar bg-neutral-800 shadow-sm">
+        <div className="flex-none">
+          <a className="btn btn-ghost text-xl">🕹️ Retro Launcher 🕹️</a>
+        </div>
+        <div className="flex-1">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <details>
+                <summary>Menu</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li><a>Link 1</a></li>
+                  <li><a>Link 2</a></li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <details>
+                <summary>Tools</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li><a>Link 1</a></li>
+                  <li><a>Link 2</a></li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <details>
+                <summary>View</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li><a>Link 1</a></li>
+                  <li><a>Link 2</a></li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <details>
+                <summary>Arrange By</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li><a>Link 1</a></li>
+                  <li><a>Link 2</a></li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <details>
+                <summary>Image Group</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li><a>Link 1</a></li>
+                  <li><a>Link 2</a></li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <details>
+                <summary>Badges</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li><a>Link 1</a></li>
+                  <li><a>Link 2</a></li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+}
